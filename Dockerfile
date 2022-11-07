b4817a3fd28c11db5bcd30f9c2ccce73e8d503e8
Ashok Pai <ashokpaik@gmail.com>
2022-11-07 17:31:21 +0530

    Docker

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,5 @@ LABEL maintainer="####"
 COPY . /Interview9
 WORKDIR /Interview9
 RUN pip install --no-cache-dir -r requirements.txt
-RUN pip install webdriver_manager
 RUN ["pytest", "-v", "--junitxml=testreports/report.xml"]
 CMD tail -f /dev/null
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 driver = None
 
@@ -8,8 +10,9 @@ driver = None
 @pytest.fixture(scope="class")
 def setup(request):
     global driver
-    chrome_obj = Service("E:\SeleniumPython\chromedriver.exe")
-    driver = webdriver.Chrome(service=chrome_obj)
+    #chrome_obj = Service("E:\SeleniumPython\chromedriver.exe")
+    #driver = webdriver.Chrome(service=chrome_obj)
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get("https://rahulshettyacademy.com/angularpractice/")
     request.cls.driver = driver
     yield
