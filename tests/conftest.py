de9941f347cd3d46a6a81e2aba2322d8dfa67f1c
Ashok Pai <ashokpaik@gmail.com>
2022-11-08 17:22:19 +0530

    Docker

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
 
@@ -9,7 +10,11 @@ driver = None
 @pytest.fixture(scope="class")
 def setup(request):
     global driver
-    chrome_obj = Service("/usr/bin/chromedriver")
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_obj = Service("/usr/bin/chromedriver", chrome_options=chrome_options)
     driver = webdriver.Chrome(service=chrome_obj)
     driver.get("https://rahulshettyacademy.com/angularpractice/")
     request.cls.driver = driver
